f5f78e951fe3995608663b53b407e3d7f857567a
Eric Durr <ericdurr@Erics-MacBook-Pro.local>
2017-02-22 10:07:40 -0700

    require fix

--- lib/etherpad_canvas.rb
+++ lib/etherpad_canvas.rb
@@ -11,7 +11,9 @@
 # GNU Affero General Public License for more details.
 
 # You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.require "etherpad_canvas/engine"
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+require "etherpad_canvas/engine"
 
 module EtherpadCanvas
 end
