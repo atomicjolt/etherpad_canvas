e345882f5a584ca40c63b1c7ebab19b6af1e66ed
Ryker Blunck <rykerblunck@gmail.com>
2017-03-08 14:15:32 -0700

    don't break if plugin is disabled

--- lib/etherpad_canvas/etherpad_collaboration.rb
+++ lib/etherpad_canvas/etherpad_collaboration.rb
@@ -21,25 +21,25 @@ class EtherpadCollaboration
 
   def self.sign_url(user, collaboration)
     plugin = PluginSetting.find_by(name: "etherpad_canvas")
-    if !plugin.disabled
-      key_pem = plugin.settings[:key]
-      key = OpenSSL::PKey::RSA.new(key_pem)
-      timestamp = (Time.now.to_f * 1000).to_i
-      query = {
-        timestamp: timestamp,
-        user_id: user.id,
-        username: user.name,
-      }.to_query
-      if collaboration.url.include?("?")
-        url = collaboration.url.split("?")
-        url[1] = signing(key, query)
-        collaboration.url = url.join
-      else
-        collaboration.url += signing(key, query)
-      end
+    return collaboration.url if plugin.disabled
 
-      collaboration.url
+    key_pem = plugin.settings[:key]
+    key = OpenSSL::PKey::RSA.new(key_pem)
+    timestamp = (Time.now.to_f * 1000).to_i
+    query = {
+      timestamp: timestamp,
+      user_id: user.id,
+      username: user.name,
+    }.to_query
+    if collaboration.url.include?("?")
+      url = collaboration.url.split("?")
+      url[1] = signing(key, query)
+      collaboration.url = url.join
+    else
+      collaboration.url += signing(key, query)
     end
+
+    collaboration.url
   end
 
   def self.signing(key, query)
